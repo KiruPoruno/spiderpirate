5d6b38674fc006b19c2b5216c4126e4166dd7cea
Kiru <mail@kiru.gay>
2022-12-18 17:55:55 +0100

    slight restructuring
    You can now search specific sources only, and also query which sources
    are available.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
+const fs = require("fs");
+const { join } = require("path");
 const express = require("express");
 const app = express();
 
 const query = require("./query");
+const settings = require("./settings");
 
-app.get("/:query", (req, res) => {
+var sources = fs.readdirSync(join(__dirname, "sources"));
+
+var filtered_sources = [];
+var sources_without_extension = [];
+for (let i = 0; i < sources.length; i++) {
+	let no_extension = sources[i].replace(/\.js$/, "");
+
+	if (! settings.proxies[no_extension]) {
+		continue;
+	}
+
+	filtered_sources.push(sources[i]);
+	sources_without_extension.push(no_extension);
+}
+
+sources = filtered_sources;
+
+app.get("/search/:query", (req, res) => {
 	query(req.params.query, (data) => {
 		return res.send(data);
 	});
+})
+
+app.get("/search/:source/:query", (req, res) => {
+	let source = req.params.source;
+	if (! sources.includes(source + ".js")) {
+		res.statusCode = "404";
+		return res.send();
+	}
+
+	let source_module = require(
+		join(__dirname, "sources", source)
+	)
+
+	source_module.search(
+		settings.proxies[source], 
+		req.params.query, (data) => {
+			return res.send(data);
+		}
+	)
+})
+
+app.get("/sources", (req, res) => {
+	return res.send(sources_without_extension);
 })
 
 let port = "15471";
