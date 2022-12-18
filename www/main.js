787fa45140c5a06968b88b78b24590c5778595d3
Kiru <mail@kiru.gay>
2022-12-18 21:21:55 +0100

    actual fully functioning frontend

--- www/main.js
+++ www/main.js
@@ -46,6 +46,98 @@ function rank_results(results) {
 	return sorted;
 }
 
+function render_results(results) {
+	results_div = document.getElementById("results");
+	results_div.innerHTML = "";
+
+	if (results.length == 0) {
+		results_div.innerHTML = `
+			<div class="padding">
+				<center>No results found</center>
+			</div>
+		`;
+
+		// hide result header
+		document.querySelector(".result").style.display = "none";
+		return;
+	}
+
+	// show result header
+	document.querySelector(".result").style.display = "flex";
+
+	let make_result = (result) => {
+		let link_domain = new URL(result.link).origin;
+
+		let content = `
+			<b>Source:</b> ${result.source_pretty}<br>
+			<b>Category:</b> ${result.category}<br>
+			<b>Link to upload:</b> <a target="_blank" href="${result.link}">${link_domain}</a><br>
+			<b>Uploader:</b> ${result.uploader}<br>
+			<b>Upload date:</b> ${result.date}<br>
+		`;
+
+		if (result.magnet) {
+			content += `
+				<br><b>Magnet Link:</b><br><br>
+				<input value="${result.magnet}" disabled>
+			`
+		}
+
+		results_div.innerHTML += `
+			<div class="result">
+				<div class="header">
+					<div class="cell">
+						${result.name}
+					</div>
+					<div class="cell">
+						${result.stats.seeders}
+					</div>
+					<div class="cell">
+						${result.stats.leechers}
+					</div>
+					<div class="cell">
+						${result.stats.size}
+					</div>
+				</div>
+				<div class="content">
+					<div class="padding">
+						${content}
+					</div>
+				</div>
+			</div>
+		`
+	}
+
+	for (let i = 0; i < results.length; i++) {
+		make_result(results[i]);
+	}
+}
+
+let delay = 1000;
+let timeout = false;
+let last_string = "";
+let input = document.getElementById("input");
+input.addEventListener("keyup", () => {
+	if (last_string == input.value) {
+		return;
+	}
+
+	if (input.value.replace(/ /g, "") == "") {
+		return;
+	}
+
+	if (timeout) {
+		clearTimeout(timeout);
+	}
+
+	timeout = setTimeout(() => {
+		last_string = input.value;
+		search((res) => {
+			render_results(rank_results(res));
+		}, input.value)
+	}, delay)
+})
+
 document.body.addEventListener("click", (e) => {
 	let element = false;
 	let elements = e.path;
@@ -64,7 +156,6 @@ document.body.addEventListener("click", (e) => {
 		}
 	}
 
-	console.log(element)
 	if (is_content || ! element) {return}
 
 	let content = element.querySelector(".content");
