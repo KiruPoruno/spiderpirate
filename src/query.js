524b7ac3f4f426a05fa15904683718c4821f5f72
Kiru <mail@kiru.gay>
2023-08-13 22:18:00 +0200

    fix using list of available proxies, not enabled
    This caused the search function to keep waiting for all proxies to be
    done searching, even ones that aren't enabled, making it never finish.

--- src/query.js
+++ src/query.js
@@ -4,6 +4,13 @@ const { join } = require("path");
 const settings = require("./settings");
 
 var sources = fs.readdirSync(join(__dirname, "sources"));
+var enabled_sources = [];
+
+for (let i in settings.proxies) {
+	if (sources.includes(i + ".js")) {
+		enabled_sources.push(i);
+	}
+}
 
 function search(query, callback = () => {}) {
 	let done = 0;
@@ -34,7 +41,7 @@ function search(query, callback = () => {}) {
 	}
 
 	let check_status = setInterval(() => {
-		if (done == sources.length) {
+		if (done == enabled_sources.length) {
 			clearInterval(check_status);
 			callback(results)
 		}
